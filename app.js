86623cce2abc30557e7902dfff35be49a7c19586
Dave <dave@Daves-MBP.lan>
2017-08-11 18:17:39 +0100

    Uses Mocha as dev dep and runs all tests from specs folder

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('ok');
-  console.log('ok');
+  res.send('200');
 });
 
 module.exports = app;
