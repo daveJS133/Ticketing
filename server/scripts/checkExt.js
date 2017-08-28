aee98244c23e04ed921a489ca6ba0e58fd524bda
daveJS133 <daveJS133@gmail.com>
2017-08-28 09:04:31 +0100

    modularize external requests

---
+++ server/scripts/checkExt.js
@@ -0,0 +1,24 @@
+function checkExternal(isAuto, initial) {
+
+  let path = urlBuilder([], requestTime.checkInitial(initial));
+  let options = {
+    hostname: 'api.edinburghfestivalcity.com',
+    port: '80',
+    path: path,
+    method:'GET',
+    headers: {'accept': 'application/json;ver=2.0'}
+  };
+ 
+  http.request(options,(res) => {
+    var body = '';
+    res.on('data',(chunk) =>{
+      body+= chunk;
+    });
+
+    res.on('end',() =>{
+      var page = JSON.parse(body);
+    });
+  }).end();
+};
+
+module.exports = checkExternal
--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const mongoose = require('mongoose');
 //Script dependencies
 const urlBuilder = require('./scripts/urlBuilder');
 const requestTime = require('./scripts/requestTime');
-
+const checkExternal = require('./scripts/checkExt')
 //For api endpoints
 server.use(compression());
 server.use('/api', api);
@@ -26,32 +26,6 @@ checkExternal(true, initial);
 setInterval(function(){
   checkExternal(true, this.initial);
 }.bind({initial:initial}), 8.64e+7, initial);
-
-
-function checkExternal(isAuto, initial) {
-
-  let path = urlBuilder([], requestTime.checkInitial(initial));
-  let options = {
-    hostname: 'api.edinburghfestivalcity.com',
-    port: '80',
-    path: path,
-    method:'GET',
-    headers: {'accept': 'application/json;ver=2.0'}
-  };
- 
-  http.request(options,(res) => {
-    var body = '';
-    res.on('data',(chunk) =>{
-      body+= chunk;
-    });
-
-    res.on('end',() =>{
-      var page = JSON.parse(body);
-    });
-  }).end();
-};
-
-
 
 module.exports = server;
 
