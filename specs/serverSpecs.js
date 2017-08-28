c21e3b4f46781daf2e13a1af98d3eb4db1f7392f
daveJS133 <daveJS133@gmail.com>
2017-08-28 10:44:35 +0100

    adds bluebird as dependency, create test environment for integration testing

--- specs/serverSpecs.js
+++ specs/serverSpecs.js
@@ -1,8 +1,29 @@
-var request = require('supertest');
-var testEvent = require('./testEvent')
-var server = require('../server/server');
+const helper = require('./server/test-helper');
+const Event = require('./server/models/event');
+const request = require('supertest-as-promised')(helper.app);
+const testEvent = require('./testEvent')
+const server = require('../server/server');
+const URL = '/api/v1/events'
 
-describe('Server Accessing API', function () {
+describe('Events', function () {
+  let event
+  before(function () {
+    return helper.dropCollection(Event)
+  });
+  it('should add an event', function () {
+    const data = testEvent
+    return request
+    .post(URL)
+    .send(data)
+    .then(function(res){
+      event = res.body
+      assert.equal(res.body.artist, data.artist)
+      assert(res.body.id);
+    })
+  });
+});
+
+describe('Server serving API', function () {
   it('Returns 200 status code', function (done) {
     request(server)
     .checkExternal(true, true)
---
+++ specs/test-helper.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const Bluebird = require('Bluebird');
+const config = require('../server/config');
+
+// Use test db
+const dbUrl = config.get('DB_URI')+'-test';
+
+config.set('DB_URI', dbURL);
+config.set('DEBUG', false);
+config.set('ENV', 'test');
+
+//Drop a collection when passed a model in test env only
+exports.dropCollection = function(Model){
+  if(config.get('ENV') !== 'test') return Bluebird.reject()
+    return Model.remove({})
+};
+
+exports.server = require('server../server/server');
+
